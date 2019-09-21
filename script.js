64e967ce7ca1103d62076fa3876d981fd08fa193
marjsimborio <55311900+marjsimborio@users.noreply.github.com>
2019-09-21 16:15:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
- alert("sample");
+var input = document.getElementById('userinput');
+var button = document.getElementById('enter');
+var ul = document.querySelector('ul');
+var listItems = document.getElementsByTagName('li')
+
+
+//add Item Function
+
+function createItem() {
+ 		var li = document.createElement('li');
+			li.innerHTML = input.value;
+			ul.appendChild(li);
+			input.value = "";
+		var delBtn = document.createElement('button');
+	delBtn.innerHTML = "Delete";
+	listItems[i].appendChild(delBtn);
+	delBtn.onclick=delItem;
+}
+
+button.addEventListener("click", function(){
+	// console.log("click is working");
+
+	if (input.value.length > 0 ) { 
+		createItem();
+		
+	}
+})
+
+input.addEventListener("keypress", function(){
+
+	if (input.value.length > 0 && event.which == 13) {
+			createItem();
+			
+	}
+
+})
+ul.onclick = function(event){
+	event.target.classList.toggle('done');
+}
+
+function createDel(){
+	var delBtn = document.createElement('button');
+	delBtn.innerHTML = "Delete";
+	listItems[i].appendChild(delBtn);
+	delBtn.onclick=delItem;
+}
+
+for(i= 0; i < listItems.length; i++) {
+	createDel();
+}
+
+function delItem (evt){
+	evt.target.parentNode.remove()
+
+}
+
+
+
+	
